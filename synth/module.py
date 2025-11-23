8f86a92ff0fa79f813298ba813b0cfed585c2d1e
Tomas <atm7812@mavs.uta.edu>
2025-11-23 02:35:51 -0600

    working on module.py and getting the signal logic

--- synth/module.py
+++ synth/module.py
@@ -16,3 +16,26 @@ keep samples betweem -1.0 and +1.0
 must be deterministic(same input = same output)
 must not modify its input in place
 '''
+
+from abc import ABC, abstractmethod
+
+class Module(ABC):
+
+    @abstractmethod
+    def generate(self, time_array):
+        pass
+
+
+class OscillatorModule(Module):
+    def __init__(self, wave, amp = 0.5):
+        self.wave = wave
+        self.amp = amp
+
+    def generate(self, time_array):
+        signal = self.amp * self.wave.generate(time_array)
+
+        return signal
+    
+
+    ## Having all files imported to a test script for the next testing run.
+    # personal note: make sure you understand every piece of code inside and out
